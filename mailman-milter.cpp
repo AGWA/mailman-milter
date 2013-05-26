c596df1ef6afe9c427b5b7dba4dbeb59d49cca92
Andrew Ayer <agwa@andrewayer.name>
2013-05-26 11:06:17 -0700

    Add legalese

--- mailman-milter.cpp
+++ mailman-milter.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2013 Andrew Ayer
+ *
+ * This file is part of mailman-milter.
+ *
+ * mailman-milter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mailman-milter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mailman-milter.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "utils.hpp"
 #include <libmilter/mfapi.h>
 #include <iostream>
